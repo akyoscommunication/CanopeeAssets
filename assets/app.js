062060e1865845eb7f2c0089564ab923f415a0e7
Lilian Carneau <lilian.akyos@gmail.com>
2023-12-12 17:37:09 +0100

    feat: modal and style

--- assets/app.js
+++ assets/app.js
@@ -3,12 +3,14 @@ import './css/app.scss';
 import './bootstrap.js';
 import Sidebar from "./js/components/Sidebar.js";
 import UsersGroup from "./js/components/UsersGroup.js";
+import Modal from "./js/components/Modal.js";
 import {FlashMessages} from "./js/components/FlashMessages.ts";
 import Tooltip from "@canopee_app/assets/js/components/Tooltip";
 
 window.addEventListener('DOMContentLoaded', () => {
 	new Sidebar();
 	new UsersGroup();
+	new Modal();
 	FlashMessages.register();
 	Tooltip.register();
 });
---
+++ assets/js/components/Modal.js
@@ -0,0 +1,30 @@
+export default class Modal {
+	constructor() {
+		document.querySelectorAll('*[trigger-modal]').forEach((el) => {
+			el.addEventListener('click', (e) => {
+				e.preventDefault();
+				const target = document.querySelector(el.getAttribute('trigger-modal'));
+				
+				if (target) {
+					// move modal to body if not already
+					if (target.parentNode !== document.body) {
+						document.body.appendChild(target);
+					}
+					
+					// block scroll
+					document.body.style.overflow = 'hidden';
+					
+					target.showModal();
+					
+					target.addEventListener('click', (e) => {
+						if (e.target === target) {
+							target.close();
+							// unblock scroll
+							document.body.style.overflow = 'auto';
+						}
+					});
+				}
+			})
+		});
+	}
+}
