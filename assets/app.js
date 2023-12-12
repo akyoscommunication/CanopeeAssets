28c54b200a6ac4407c6e75bd4a98af45b1362360
Lilian Carneau <lilian.akyos@gmail.com>
2023-12-12 16:10:15 +0100

    feat: add icons and users groups

--- assets/app.js
+++ assets/app.js
@@ -2,11 +2,13 @@ import './css/app.scss';
 
 import './bootstrap.js';
 import Sidebar from "./js/components/Sidebar.js";
+import UsersGroup from "./js/components/UsersGroup.js";
 import {FlashMessages} from "./js/components/FlashMessages.ts";
 import Tooltip from "@canopee_app/assets/js/components/Tooltip";
 
 window.addEventListener('DOMContentLoaded', () => {
 	new Sidebar();
+	new UsersGroup();
 	FlashMessages.register();
 	Tooltip.register();
 });
