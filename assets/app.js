1b5c23dc38c34b1d4cc420d0f6e84192e3bcf97c
Groux kévin <kevin.akyos@gmail.com>
2024-03-07 14:52:55 +0100

    feat: only css helpinfo

--- assets/app.js
+++ assets/app.js
@@ -6,18 +6,11 @@ import Impersonate from "./js/components/Impersonate.js";
 import Modal from "./js/components/Modal.js";
 import {FlashMessages} from "./js/components/FlashMessages.ts";
 import Tooltip from "@canopee_app/assets/js/components/Tooltip";
-import HelpInfo from "@canopee_app/assets/js/components/HelpInfo";
 
 window.addEventListener('DOMContentLoaded', () => {
 	new Sidebar();
 	new Modal();
 	new Impersonate();
-
-	// C'est pas ouf, voir pour init HelpInfo au changement live sur une page (exemple: bouton editer qui affiche le from et fonc les helpInfo ne sont pas init)
-	window.addEventListener('click', () => {
-		new HelpInfo();
-	})
-
 	FlashMessages.register();
 	Tooltip.register();
 });
