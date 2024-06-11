521a0ad6e2f6626eaa717b4594e9289bd8ff120b
Lilian Carneau <lilian.akyos@gmail.com>
2024-06-11 10:49:19 +0200

    fix: remove widget style and add contrainte to card

--- src/Twig/Components/CardComponent.php
+++ src/Twig/Components/CardComponent.php
@@ -7,4 +7,5 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent('card', template: '@CanopeeAssets/components/card.html.twig')]
 final class CardComponent
 {
+    public string $tag = 'div';
 }
