a55b46af0816e30eb7832f967a47231a5ba97c40
Thomas Sebert <thomas.sebert.akyos@gmail.com>
2024-10-10 14:20:14 +0200

    Remove unused files

--- src/DependencyInjection/CanopeeAssetsExtension.php
+++ src/DependencyInjection/CanopeeAssetsExtension.php
@@ -12,7 +12,6 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class CanopeeAssetsExtension extends Extension implements PrependExtensionInterface
 {
-
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
