4f38a112ba793f4134c80ec2cb8353c8b2fa4cd9
Thomas Sebert <thomas.sebert.akyos@gmail.com>
2024-11-07 14:03:49 +0100

    Fix collaborator card

--- src/Twig/Components/CollaboratorCardComponent.php
+++ src/Twig/Components/CollaboratorCardComponent.php
@@ -2,13 +2,13 @@
 
 namespace Akyos\CanopeeAssets\Twig\Components;
 
-use App\Entity\User;
+use App\Entity\UserAccessRight;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent(name: 'collaborator_card', template: '@CanopeeAssets/components/collaborator/card.html.twig')]
 final class CollaboratorCardComponent
 {
-    public User $collaborator;
+    public UserAccessRight $userAccessRight;
 
     public ?string $tag = 'a';
 }
