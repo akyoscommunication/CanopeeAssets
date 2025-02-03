5accfe9e0cd2e15c1fd9acfba9f876904f16b375
Lilian Carneau <lilian.akyos@gmail.com>
2025-02-03 11:18:57 +0100

    fix: galaxy map + style

--- assets/js/components/Galaxy/ui/List.tsx
+++ assets/js/components/Galaxy/ui/List.tsx
@@ -6,7 +6,18 @@ import ListItem from "./ListItem";
 export default function List() {
     const { data } = React.useContext(GalaxyContext);
 
-    return <ul className="l-grid l-grid--4 l-grid--2:lg l-grid--1:md">
-        {data?.map((item, index) => <li key={index}><ListItem item={item}/></li>)}
-    </ul>
+    const customers = Object.keys(data);
+
+    return <div className="c-modal__body">
+        {customers.map((customer, index) => {
+            return <div key={index} className="c-widget">
+                <div className="c-widget__header">
+                    <h3 className="c-title c-title--h3">{customer}</h3>
+                </div>
+                <ul className="l-grid l-grid--4 l-grid--2:lg l-grid--1:md">
+                    {data[customer].map((item, index) => <li key={index}><ListItem item={item}/></li>)}
+                </ul>
+            </div>
+        })}
+    </div>
 }
